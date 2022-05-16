5e6db164ff997dcbc69db59b0460cfc2d25f6a73
peksinsarae <peksin.sara@gmail.com>
2022-05-16 12:37:59 +0200

    added hero and started configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+//Rineeby network
+const supportedChainIds=[4]
+
+//connection method used by Metamask
+const connectors={
+  injected:{}
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
